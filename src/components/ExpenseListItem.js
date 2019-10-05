3abd29351441fd2ff9e72aeb0983fba0a05d4e11
nedilio <izquierdonelson@gmail.com>
2019-10-05 20:36:44 -0300

    formateando fechas y montos en dashboard

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+
+numeral.register('locale', 'cl', {
+  delimiters: {
+      thousands: '.',
+      decimal: ','
+  },
+  abbreviations: {
+      thousand: 'k',
+      million: 'm',
+      billion: 'b',
+      trillion: 't'
+  },
+  currency: {
+      symbol: '$'
+  }
+});
+
+// switch between locales
+numeral.locale('cl');
 
 const ExpenseListItem = ({ description, amount, createdAt, id}) => (
   <div>
-    <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
-    <p>{amount} - {createdAt}</p>
+    <Link to={`/edit/${id}`}>
+    <h3>{description}</h3></Link>
+    <p>{numeral(amount).format('$ 0.0[,]00')}
+    - 
+    {moment(createdAt).format('D MMMM, YYYY')}</p>
   </div>
 );
 
