ccd6a3e25e185b0ba4497899495f07a9652130a1
nedilio <izquierdonelson@gmail.com>
2019-10-07 01:11:04 -0300

    expenses total test (fails)

---
+++ src/tests/selectors/expenses-total.test.js
@@ -0,0 +1,15 @@
+import selectExpensesTotal from '../../selectors/expenses-total';
+import expenses from '../fixtures/expenses';
+
+test(' return 0 if no expenses', () => {
+  const res = selectExpensesTotal([]);
+  expect(res).toBe(0);
+});
+test(' add 1 expense', () => {
+  const res = selectExpensesTotal([expenses[0]]);
+  expect(res).toBe(500);
+});
+test(' multiple expenses total', () => {
+  const res = selectExpensesTotal([expenses]);
+  expect(res).toBe(355000+277000+500);
+});
