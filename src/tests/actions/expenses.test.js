1fadc86586017b6c9fd1cee8d0cb79e864f7dd28
nedilio <izquierdonelson@gmail.com>
2019-10-16 18:58:44 -0300

    Datos de firebase

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,10 +1,18 @@
-import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
+
+beforeEach(() => {
+  const expensesData = {};
+  expenses.forEach(({ id, description, note, amount, createdAt }) => {
+    expensesData[id] = { description,note,amount,createdAt}
+  });
+  database.ref('expenses').set(expensesData).then(() => done());
+});
 
 test('setup remove expense action object', () => {
   const action = removeExpense({id: '123abc'});
@@ -50,7 +58,6 @@ test('add expense to database and store', (done) => {
   }
   store.dispatch(startAddExpense(expenseData)).then(() => {
     const actions = store.getActions();
-    console.log(actions[0]);
     expect(actions[0]).toEqual({
       type: 'ADD_EXPENSE',
       expense: {
@@ -77,7 +84,7 @@ test('add expense with defaults to database and store', () => {
   }
   store.dispatch(startAddExpense({})).then(() => {
     const actions = store.getActions();
-    console.log(actions[0]);
+    // console.log(actions[0]);
     expect(actions[0]).toEqual({
       type: 'ADD_EXPENSE',
       expense: {
@@ -105,3 +112,24 @@ test('add expense with defaults to database and store', () => {
 //     }
 //   });
 // });
+
+test('setup expense action object with data', () => {
+  const action = setExpenses(expenses);
+  expect(action).toEqual({
+    type: 'SET_EXPENSES',
+    expenses
+  })
+});
+
+test('should fetch the expenses from firebase', () => {
+  const store = createMockStore({});
+  store.dispatch(startSetExpenses()).then(()=>{
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type:'SET_EXPENSES',
+      expenses
+    });
+    done();
+  })
+  .catch( console.log('error fetching data'));
+});
